553cb8b8ddf1167513e4981b73df65af1e67e279
@Eva-87 <mariaeva_martin@hotmail.com>
2026-01-27 14:44:56 +0100

    chore: create repository and start Board class

---
+++ src/main/java/com/tictactoe/Board.java
@@ -0,0 +1,26 @@
+package com.tictactoe;
+
+public class Board { 
+    private char[][][] cells={" ", " ", " "},{" ", " "," "},{" ", " "," "};
+        
+    
+        public void printBoard(){
+
+        }
+
+        public void addMove(){
+
+        }
+
+        public void checkCell(){
+
+        }
+         public void checkWin(){
+
+         }
+
+         public void checkFullBoard(){
+            
+         }
+
+}
