386e70b9225e99cf5f4bc8c8e3e88bdcd2178dde
Guadalupe <penaegeaguadalupe@gmail.com>
2026-01-28 14:24:51 +0100

    feat:Player class

--- src/main/java/com/tictactoe/Players.java
+++ src/main/java/com/tictactoe/Players.java
@@ -2,4 +2,50 @@ package com.tictactoe;
 
 public class Players {
 
+    private String name;
+    private String rol;// investigar enums añadimos color
+    private int win;
+
+    // Constructors
+
+    public Players(String name) {
+        this.name = name;
+    }
+
+
+    // Getters&Setters
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRol() {
+        return this.rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public int getWin() {
+        return this.win;
+    }
+
+    public void setWin(int win) {
+        this.win = win;
+    }
+
+    
+    // Methods 
+
+    public void move(){
+
+                //addMove()
+                //checkCell()
+    }
+
 }
