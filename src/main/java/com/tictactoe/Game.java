63fecf304d674babdb2771eb8d31ad7c31e2a91e
Nuclea88 <ingrid.lopov@gmail.com>
2026-01-28 14:54:52 +0100

    feat: new game class

---
+++ src/main/java/com/tictactoe/Game.java
@@ -0,0 +1,39 @@
+package com.tictactoe;
+
+public class Game {
+
+    private Player PlayerX;
+    private Player PlayerO;
+    private Board Board;
+
+    //Constructor
+    public Game(){}
+
+        //Getters &setters
+
+
+        //métodos
+    public void rules(){
+
+    }
+
+    public void designatePlayers(Player player1, Player player2){
+        //Math.random()*2;
+        //asignar jugadores: simbolo y color x defecto
+    }
+    
+    public void startGame(){
+
+        //bucle del juego
+        //scanner global?? como metemos sino los inputs de movimiento??
+
+
+    }
+
+    public void checkRange(){
+            //verifica el rango y TRADUCE al array
+
+    }
+
+
+}
